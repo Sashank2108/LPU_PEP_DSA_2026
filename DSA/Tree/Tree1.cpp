74f138723ba8514389c75edf558a21b56e600463
Sashank2108 <sashankpalla9@gmail.com>
2026-02-13 22:05:48 +0530

    Trees

---
+++ DSA/Tree/Tree1.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class TreeNode{
+    public:
+    int data;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int val){
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+int main(){
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    root->right->left = new TreeNode(6);
+    root->right->right = new TreeNode(7);
+
+}
