ae017cd5cd913e7d864bfd81d58582ff82c976ac
Sashank2108 <sashankpalla9@gmail.com>
2026-02-16 23:14:49 +0530

    Traversals

--- DSA/Tree/Tree1.cpp
+++ DSA/Tree/Tree1.cpp
@@ -14,6 +14,33 @@ class TreeNode{
     }
 };
 
+void inorder(TreeNode* root){
+    if(root == nullptr){
+        return;
+    }
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}
+
+void preorder(TreeNode* root){
+    if(root == nullptr){
+        return;
+    }
+    cout<<root->data<<" ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void postorder(TreeNode* root){
+    if(root == nullptr){
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    cout<<root->data<<" ";
+}
+
 int main(){
     TreeNode* root = new TreeNode(1);
     root->left = new TreeNode(2);
@@ -22,5 +49,11 @@ int main(){
     root->left->right = new TreeNode(5);
     root->right->left = new TreeNode(6);
     root->right->right = new TreeNode(7);
-
+    cout << "Preorder Traversal : ";
+    preorder(root);
+    cout << endl << "Inorder Traversal: ";
+    inorder(root);
+    cout << endl << "Postorder Traversal: ";
+    postorder(root);
+    cout << endl;
 }
