b8268202b35aa0fd97f277140bb6880dc420cad0
Sashank2108 <sashankpalla9@gmail.com>
2026-02-18 22:35:32 +0530

    Stack Files

--- DSA/Stack/Stack1.cpp
+++ DSA/Stack/Stack1.cpp
@@ -1,6 +1,54 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+class MyStack{
+    public:
+    Node* top;
+
+    MyStack(){
+        top = NULL;
+    }
+
+    void push(int val){
+        Node* node = new Node(val);
+        node->next = top;
+        top = node;
+    }
+
+    void pop(){
+        if(top == NULL){
+            cout << "Stack underflow" << endl;
+            return;
+        }
+        Node* temp = top;
+        top = top->next;
+        delete temp;
+    }
+
+    int peek(){
+        if(top == NULL){
+            cout << "Stack is Empty "<< endl;
+            return;
+        }
+        return top->data;
+    }
+
+    bool isEmpty(){
+        return top == NULL;
+    }
+};
+
 int main(){
     stack<int> st;
     st.push(1);
