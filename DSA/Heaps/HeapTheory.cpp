7dcf932db566b6065363911ac82756e23e732db3
Sashank2108 <sashankpalla9@gmail.com>
2026-02-19 13:47:39 +0530

    Heap Theory

---
+++ DSA/Heaps/HeapTheory.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+    Heaps:- A heap is a complete binary tree that follows the heap order property.
+        - Complete Binary Tree:- All levels are completely filled except the last level, the last level
+                                 is filled from left to right.
+
+        - Heap Order Property:- 1. Max Heap -> Every parent node is greater than or equal to its 
+                                               childern, the largest element is always at root.
+                                2. Min Heap -> Every parent node is smaller that or equal to its
+                                               children the smallest element is always at top.
+
+    *Heap is not a Binary Search Tree(BST).
+
+    -> For 1-based 'i' index of heap:- 1. Left child = 2*i; where i is index of that element.
+                                       2. Right child = 2*i + 1  
+                                       3. Parent = i/2
+            
+    -> For 0-based 'i' index of heap:- 1. Left child = 2*i + 1
+                                       2. Right child = 2*i + 2
+                                       3. Parent = (i-1)/2
+*/
+
+int main(){
+    
+    return 0;
+}
