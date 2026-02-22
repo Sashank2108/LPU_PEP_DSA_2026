c7564d78ddbc8ea7ba8ea22ad4acc6324008a471
Sashank2108 <sashankpalla9@gmail.com>
2026-02-22 21:10:59 +0530

    Heap Implementation

--- DSA/Heaps/HeapImplementation.cpp
+++ DSA/Heaps/HeapImplementation.cpp
@@ -48,36 +48,7 @@ class MaxHeap{
     }
 };
 
-void heapify(vector<int> &arr, int n, int i){
-    int largest = i;
-    int left = 2 * i + 1;
-    int right = 2 * i + 2;
-    if(left < n && arr[left] > arr[largest])
-        largest = left;
-    if(right < n && arr[right] > arr[largest])
-        largest = right;
-    if(largest != i){
-        swap(arr[i], arr[largest]);
-        heapify(arr, n, largest);
-    }
-}
-void maxHeap(vector<int> &arr, int n){
-    for(int i = n/2;i>=0;i--){
-        heapify(arr, n, i);
-    }
-}
 
 int main(){
-    vector<int> arr = {10, 3, 5, 30, 2, 8, 15};
-    int n = arr.size();
-    cout << "Original Array" << endl;
-    for(int i:arr)
-        cout << i << " ";
-    cout << endl;
-    maxHeap(arr, n);
-    cout << "Max Heap Array" << endl;
-    for(int i:arr)
-        cout << i << " ";
-    cout << endl;
-    return 0;
+    
 }
