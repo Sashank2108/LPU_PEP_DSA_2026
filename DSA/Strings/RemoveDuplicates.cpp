a7a10d8b8b6940899a4f119a83da02956967c6c5
Sashank2108 <sashankpalla9@gmail.com>
2026-03-02 20:04:15 +0530

    Remove Duplicates

--- DSA/Strings/RemoveDuplicates.cpp
+++ DSA/Strings/RemoveDuplicates.cpp
@@ -1,9 +1,18 @@
-    // Remove Dupilcates
-    /* string str = "HhHHeeelloooowW";
+#include<bits/stdc++.h>
+using namespace std;
+
+void removeDuplicates(string str){    
     int n = str.length();
     string newStr = "";
     for(int i=0;i<n;i++){
         if(newStr.find(str[i])==-1)
             newStr += str[i];
     }
-    cout << newStr; */
+    cout << newStr; 
+}
+
+int main(){
+    string str = "HhHHeeelloooowW";
+    removeDuplicates(str);
+    return 0;
+}
