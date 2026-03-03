a60142bd905047c40a0fd7b1c153177c178e6864
Sashank2108 <sashankpalla9@gmail.com>
2026-03-03 21:19:36 +0530

    2D Array

--- CPP/1.cpp
+++ CPP/1.cpp
@@ -37,22 +37,6 @@ int main(){
     // int s = sum(arr, size);
     // cout << "Sum of the elements in the array is: " << s << endl;
 
-    // 2-D Array
-    int arr[4][5] = {{1,2,3,4,5}, {6,7,8,9,10}, {3,2,7,8,3}, {6,2,1,6,3}};
-    int row = 4, col = 5;
-    // int maxCol = INT8_MIN;
-    // int index = -1;
-    // for(int i = 0;i < 5;i++){
-    //     int sum = 0;
-    //     for(int j=0;j<4;j++){
-    //         sum+=arr[j][i];
-    //     }
-    //     if(sum>maxCol){
-    //         maxCol = sum;
-    //         index = i;
-    //     }
-    // }
-    // cout << index << endl;
 
     // wave print in 2D Array
     /*for(int col=0;col<5;col++){
@@ -64,6 +48,8 @@ int main(){
     }*/
 
     // Spiral Print
+    int arr[4][5] = {{1,2,3,4,5}, {6,7,8,9,10}, {3,2,7,8,3}, {6,2,1,6,3}};
+    int row = 4, col = 5;
     int topRow = 0, bottomRow = row-1, leftCol = 0, rightCol = col-1;
     // while(topRow<=bottomRow && leftCol<=rightCol){
     //     for(int i=leftCol;i<=rightCol;i++){
---
+++ DSA/Arrays/2dArrayMaxCol.cpp
@@ -0,0 +1,16 @@
+// 2-D Array
+    int arr[4][5] = {{1,2,3,4,5}, {6,7,8,9,10}, {3,2,7,8,3}, {6,2,1,6,3}};
+    int row = 4, col = 5;
+    // int maxCol = INT8_MIN;
+    // int index = -1;
+    // for(int i = 0;i < 5;i++){
+    //     int sum = 0;
+    //     for(int j=0;j<4;j++){
+    //         sum+=arr[j][i];
+    //     }
+    //     if(sum>maxCol){
+    //         maxCol = sum;
+    //         index = i;
+    //     }
+    // }
+    // cout << index << endl;
