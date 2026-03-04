1b7a0b060cdd4c8ea3113a565c331c7c215bbe76
Sashank2108 <sashankpalla9@gmail.com>
2026-03-04 22:29:41 +0530

    Wave Print

--- CPP/1.cpp
+++ CPP/1.cpp
@@ -38,14 +38,7 @@ int main(){
     // cout << "Sum of the elements in the array is: " << s << endl;
 
 
-    // wave print in 2D Array
-    /*for(int col=0;col<5;col++){
-        for(int row=0;row<4;row++){
-            if(col%2!=0)
-                cout << arr[4-row-1][col] << " ";
-            else cout << arr[row][col] << " ";
-        }
-    }*/
+
 
     // Spiral Print
     int arr[4][5] = {{1,2,3,4,5}, {6,7,8,9,10}, {3,2,7,8,3}, {6,2,1,6,3}};
---
+++ DSA/Arrays/2dArrayWavePrint.cpp
@@ -0,0 +1,10 @@
+    int arr[4][5] = {{1,2,3,4,5}, {6,7,8,9,10}, {3,2,7,8,3}, {6,2,1,6,3}};
+    int row = 4, col = 5;
+        // wave print in 2D Array
+    /*for(int col=0;col<5;col++){
+        for(int row=0;row<4;row++){
+            if(col%2!=0)
+                cout << arr[4-row-1][col] << " ";
+            else cout << arr[row][col] << " ";
+        }
+    }*/
