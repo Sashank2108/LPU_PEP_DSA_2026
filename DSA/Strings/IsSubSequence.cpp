cc6b2d0d6bf581ea179472812ad09be1d172a7d6
Sashank2108 <sashankpalla9@gmail.com>
2026-03-09 22:37:39 +0530

    Subsequence Checker

---
+++ DSA/Strings/IsSubSequence.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+// https://leetcode.com/problems/is-subsequence/
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i = 0, j = 0;
+
+        int n = s.length();
+        int m = t.length();
+
+        while(i < n && j < m){
+            if(s[i] == t[j]){
+                i++;
+            } 
+            j++;
+        }
+        return (i == n);
+    }
+};
